6dd40d7177bee715de209af4dcd729d46921b954
pj <Pinjiang1220@gmail.com>
2024-05-21 22:47:38 +0800

    Add seat ID in seatSchema

--- models/seatModel.js
+++ models/seatModel.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 const seatSchema = new mongoose.Schema({
+    session_id: Number,
+    area_id: Number,
+    seat: Number,
     status: Number,
     token: String,
     expire: Date
