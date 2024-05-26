1b0fef8bf2379008e8434a180891debdbcd42ece
chengyenn <a0987206119@gmail.com>
2024-05-26 16:21:06 +0800

    set up NextUI

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import { NextUIProvider } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <NextUIProvider>
+      <main>
+        <h1>Welcome to TicketBuzz</h1>
+        <Button>Get Started</Button>
+      </main>
+    </NextUIProvider>
+  );
 }
