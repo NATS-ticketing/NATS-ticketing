bc3688849e99955663344f8224ccebfa47d69b29
pj <Pinjiang1220@gmail.com>
2024-06-06 23:06:04 +0800

    Separate clear feature

--- backend/README.md
+++ backend/README.md
@@ -9,19 +9,15 @@ MONGO_URL=MONGO_URL
 
 ## Run
 
-### Simple run (single instance)
-```sh
-npm start
-```
-
-### Run using docker-compose (support mutiple instances)
-- `compose.yml` 的 `replicas` 定義了有幾個 instance，預設是 6
+### Run using docker-compose
+- `compose.yml` 的 `replicas` 定義了有幾個 instance
 
 ```sh
+# 啟動 6 個 server (6 為預設數量)
+docker-compose up -d --build
+
 # 啟動 1 個 server
 REPLICAS=1 docker-compose up -d --build
-# 啟動 6 個 server (預設數量)
-docker-compose up -d --build
 ```
 
 
---
+++ backend/main_clear.js
@@ -0,0 +1,44 @@
+/**
+ * Clear service main process
+ */
+import { connect } from "nats";
+import { createNotifyController } from "./controllers/notifyController.js";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { EventEmitter } from "events";
+
+dotenv.config({
+    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
+console.log(process.env.MONGO_URL);
+
+mongoose.connect(`${process.env.MONGO_URL}`, { dbName: "ticketing" })
+    .catch(err => console.log(err));
+
+mongoose.connection.on('connected', () => console.log('Mongo connected'));
+mongoose.connection.on('disconnected', () => console.log('Mogo disconnected'));
+
+const servers = process.env.NATS_URL;
+const nc = await connect({
+    servers: servers.split(","),
+});
+
+let emitter = new EventEmitter();
+
+emitter.on("clear", function (session, area, num) {
+    console.log("receive clear event", session, area, num);
+    const subject = `ticketing.${session}.notify.${area}`;
+    const payload = { "empty": num };
+    nc.publish(subject, JSON.stringify(payload));
+});
+
+emitter.on("start", function (session) {
+    console.log("receive start event", session,);
+    const subject = `ticketing.${session}.notify.*`;
+    const payload = { "message": "Session is starting ticket sales." };
+    nc.publish(subject, JSON.stringify(payload));
+});
+
+await createNotifyController(emitter);
+console.log("clear service started");
